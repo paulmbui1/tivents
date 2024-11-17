ae989b90da077b697a9f27d67f8f56e0464fe10b
MP <paulmbui20@gmail.com>
2024-11-17 09:26:23 +0300

    eventsBooking system with django -events listing and events details

--- events/static/js/script.js
+++ events/static/js/script.js
@@ -1,4 +1,22 @@
-
+document.addEventListener('DOMContentLoaded', function () {
+    const swiper = new Swiper('.swiper', {
+      loop: true,
+      slidesPerView: 1, // Adjust number of slides per view
+      spaceBetween: 10,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      autoplay: {
+        delay: 3000, // 3 seconds
+        disableOnInteraction: false,
+      },
+    });
+  });
   function scrollToTop() {
     window.scrollTo({
       top: 0,
